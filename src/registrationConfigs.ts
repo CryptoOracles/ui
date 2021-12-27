bedd6d8506d7036a0d5b782fa28bd817ce341062
Andrea <andrea.dhelpra19@gmail.com>
2021-12-27 18:11:58 +0000

    Asset minting set-up

---
+++ src/registrationConfigs.ts
@@ -0,0 +1,282 @@
+const NFT = {
+    publicKey: "0x047359452f4ca88e86c74002f7755162afcd31ad8482bf05dfb9cb281e488a9fca9d3d49f81f212e51fbcb3de5f09e88dab9d8924bb211875793433b5bd6289ead",
+    metadataUri: "https://gateway.pinata.cloud/ipfs/QmPwPpx4ndPCq8JcY2R71dNskSiLg8MdXg99aLPuVqEqjd/",
+    contractAddress: "0x100bf5f724a7f0dc0c4c5107b3fa82f7ec4d03c7",
+    metadataSchema: {
+        "metadata": [
+          {
+            "name": "name",
+            "type": "text"
+          },
+          {
+            "name": "description",
+            "type": "text"
+          },
+          {
+            "name": "image",
+            "type": "text"
+          },
+          {
+            "name": "image_url",
+            "type": "text"
+          },
+          {
+            "name": "dna",
+            "type": "text"
+          },
+          {
+            "name": "edition",
+            "type": "continuous"
+          },
+          {
+            "name": "date",
+            "type": "continuous"
+          },
+          {
+            "name": "compiler",
+            "type": "text"
+          },
+          {
+            "name": "Background",
+            "type": "enum",
+            "value": [
+              "clouds",
+              "dark",
+              "green",
+              "grey",
+              "moon",
+              "purle",
+              "red",
+              "stars",
+              "sun"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Background feature",
+            "type": "enum",
+            "value": [
+              "abyss",
+              "arrows",
+              "bones",
+              "columns",
+              "dunes",
+              "iceberg",
+              "meteors",
+              "moon",
+              "mountains",
+              "ocean",
+              "olympus",
+              "poles",
+              "sfondo nuvole",
+              "thunder",
+              "tree",
+              "volcano",
+              "wall",
+              "wall",
+              "waterfalls"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Body",
+            "type": "enum",
+            "value": [
+              "alien",
+              "base",
+              "droid",
+              "golden",
+              "green",
+              "purple"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Arm",
+            "type": "enum",
+            "value": [
+              "bandage",
+              "braccio filo spinato",
+              "chains",
+              "fish",
+              "gladiator",
+              "ice",
+              "leeches",
+              "magma",
+              "snake",
+              "wood"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Body feature",
+            "type": "enum",
+            "value": [
+              "arrows",
+              "bdsm_f",
+              "bdsm_m",
+              "bust",
+              "hair",
+              "roman_armour",
+              "templar",
+              "toga",
+              "togaf",
+              "wings"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Eyes",
+            "type": "enum",
+            "value": [
+              "blood",
+              "dot",
+              "ecstasy",
+              "feline",
+              "fire",
+              "goat",
+              "hypno",
+              "shark",
+              "snake"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Nose",
+            "type": "enum",
+            "value": [
+              "basic",
+              "bulky",
+              "curvy",
+              "pointy",
+              "thin"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Face feature",
+            "type": "enum",
+            "value": [
+              "bag",
+              "bandage",
+              "bandages",
+              "brain",
+              "cheek",
+              "fangs",
+              "frekles",
+              "long_hair",
+              "piercings",
+              "prostetic",
+              "robotic_cheek",
+              "sharp_cut",
+              "skeleton",
+              "snakes",
+              "tatoos",
+              "third_eye",
+              "tongue",
+              "viking_beard",
+              "worms"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Hood",
+            "type": "enum",
+            "value": [
+              "armour",
+              "crocodile",
+              "fur",
+              "jeans",
+              "jeans_green",
+              "jeans_pink",
+              "leather",
+              "tiger",
+              "whool",
+              "zip"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Head top",
+            "type": "enum",
+            "value": [
+              "arrow",
+              "aura",
+              "axe",
+              "crown",
+              "grapes",
+              "helmet",
+              "horns",
+              "laurel",
+              "minotauro",
+              "satir"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Coin",
+            "type": "enum",
+            "value": [
+              "ada",
+              "atom",
+              "avax",
+              "btc",
+              "cro",
+              "crypto_oracles",
+              "doge",
+              "dot",
+              "eth",
+              "luna",
+              "shiba",
+              "solana",
+              "tether",
+              "xcad",
+              "xrp"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Sphere",
+            "type": "enum",
+            "value": [
+              "apple_green",
+              "blue",
+              "eye",
+              "fire",
+              "green",
+              "honey",
+              "light_blue",
+              "orange",
+              "pink",
+              "planet",
+              "poison",
+              "preistoric_egg",
+              "purple",
+              "red",
+              "white",
+              "yellow"
+            ],
+            "filterable": true
+          },
+          {
+            "name": "Hand",
+            "type": "enum",
+            "value": [
+              "armour",
+              "bird",
+              "burned",
+              "fish",
+              "gipsy",
+              "glove",
+              "goat",
+              "mummy",
+              "skeleton",
+              "snake",
+              "tentacle"
+            ],
+            "filterable": true
+          }
+        ]
+      }
+ }
+ 
